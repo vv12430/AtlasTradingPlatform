022050f8698dec9203b4ac8baf6975ea1f92a9d7
Vivek <vivekv.1243@gmail.com>
2026-09-24 12:04:54 -0400

    added default value for not null columns in trades table

--- atlas-trading/portfolio-service/src/main/resources/db/migration/V2__partial_fills_schema.sql
+++ atlas-trading/portfolio-service/src/main/resources/db/migration/V2__partial_fills_schema.sql
@@ -1,6 +1,6 @@
 -- Add support for partial fills, cancellations, and fees
 alter table trades add filled_quantity number(19,6) default 0 not null;
-alter table trades add remaining_quantity number(19,6) not null;
+alter table trades add remaining_quantity number(19,6) default 0 not null;
 alter table trades add fees number(19,2) default 0 not null;
 alter table trades add cancelled_at timestamp;
 alter table trades add cancellation_reason varchar2(200);
